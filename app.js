f7a73c88948217b2393266491deed8fd14c626c4
anandhuRaveendran <anandhuraveendrank@gmail.com>
2024-06-24 12:06:11 +0530

    env imported

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 const express=require('express');
 const app=express();
 const routes=require('./routes.js')
- 
+const dotenv=require('dotenv')
 const port=3001;
 const mongoose=require('mongoose')
 app.listen(port,()=>{
     console.log(`server is runnning on port ${port}`)
 });
+
 app.use(express.json());
 app.use('/',routes);
-
+dotenv.config();
+const uri=process.env.mongodb_uri;
 mongoose.connect(
-    "mongodb://localhost:27017/UserDetails"
+    uri   
 );
 const database = mongoose.connection;
 database.on("error",(error)=>{
